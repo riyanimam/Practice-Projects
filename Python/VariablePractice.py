5ff5b654910c82024889581ce027888e902284df
Riyan Imam <riyanimam1997@gmail.com>
2016-12-12 16:18:24 -0500

    Update VariablePractice.py

--- Python/VariablePractice.py
+++ Python/VariablePractice.py
@@ -1,4 +1,4 @@
-#Variables being instantiated 
+#Variable practice
 #This was uploaded to GitHub
 
 num = 100
@@ -8,6 +8,9 @@ words = ["Hello", "there!"]
 adding = 4 + num
 number, letter, decimal = 45, "Wow!", 4.5
 
+#Tuples are read-only; think of tuples like an array in Java
+tuple1 = ("hello", 123, "wow!", 543)
+
 print num
 print letter
 print word
@@ -16,3 +19,35 @@ print adding
 print number
 print letter
 print decimal
+
+var1 = 0
+var2 = 1
+var3 = 2
+
+print var1
+print var2
+
+del var1, var3
+
+print word[0:2]
+
+print words[0]
+print words * 3
+print tuple1
+print tuple1[0:2]
+print tuple1 * 2
+
+dic1 = {
+  "history":123,
+  458:"land",
+  "go":word
+}
+
+dic1["same"] = "here"
+dic1[123] = 456
+
+print dic1
+print dic1.keys()
+print dic1.values()
+print dic1["history"]
+
