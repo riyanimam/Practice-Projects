8c32883730cb7f0cd45bde10dc64dac9ce3eff02
Riyan Imam <riyanimam1997@gmail.com>
2016-12-12 15:42:35 -0500

    Add files via upload

---
+++ Python/VariablePractice.py
@@ -0,0 +1,17 @@
+#Variables being instantiated 
+
+num = 100
+letter = 'a'
+word = "Well"
+words = ["Hello", "there!"]
+adding = 4 + num
+number, letter, decimal = 45, "Wow!", 4.5
+
+print num
+print letter
+print word
+print words
+print adding
+print number
+print letter
+print decimal
